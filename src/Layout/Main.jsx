8965bd723175ffd9acd12a341b09e07517a31dc0
bijoymamud <bijoymamud.09@gmail.com>
2024-08-20 12:49:29 +0600

    project initialization

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
+function componentName() {
+    return (
+        <div>
+            <Outlet/>
+        </div>
+    )
+}
+
+export default componentName
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+    return (
+        <div>
+           <p className='text-4xl font-extrabold'>This is some</p>
+        </div>
+    )
+}
+
+export default Home
